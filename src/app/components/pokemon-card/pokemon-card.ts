0ad04755e7cf1ba2c8f15003bfa8dd90c6714672
Ricardo Villalobos Vargas <ricardo_villalobosv@hotmail.com>
2025-08-22 16:01:21 -0500

    Change pokemon-card

--- src/app/components/pokemon-card/pokemon-card.ts
+++ src/app/components/pokemon-card/pokemon-card.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { TitleCasePipe } from '@angular/common';
+import { SerializationPipe } from '@core/pipes/serialization-pipe';
+import { Pokemon } from '@core/models/pokemon.model';
 
 @Component({
   selector: 'app-pokemon-card',
-  imports: [],
+  imports: [SerializationPipe, TitleCasePipe],
   templateUrl: './pokemon-card.html',
-  styleUrl: './pokemon-card.scss'
+  styleUrl: './pokemon-card.scss',
 })
 export class PokemonCard {
-
+  pokemon = input.required<Pokemon>();
 }
