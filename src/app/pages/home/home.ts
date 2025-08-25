b059b3afad252ec03d783565ca1d70020de8b883
Ricardo Villalobos Vargas <ricardo_villalobosv@hotmail.com>
2025-08-25 11:19:32 -0500

    add btn view more

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,7 +1,8 @@
-import { Component, inject, OnInit, signal } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit, signal } from '@angular/core';
+import { PokemonCard } from '@components/pokemon-card/pokemon-card';
 import { Pokemon } from '@core/models/pokemon.model';
 import { PokemonServices } from '@core/services/pokemon-services';
-import { PokemonCard } from '../../components/pokemon-card/pokemon-card';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -9,20 +10,44 @@ import { PokemonCard } from '../../components/pokemon-card/pokemon-card';
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
-export default class Home implements OnInit {
+export default class Home implements OnInit, OnDestroy {
+  private servicesSubscription: Subscription | undefined;
   private pokemonServices = inject(PokemonServices);
+
   private offset = signal<number>(0);
   private limit = signal<number>(15);
+  private error = signal<string | null>(null);
+
+  public loading = signal<boolean>(false);
   protected pokemons = signal<Pokemon[]>([]);
 
   ngOnInit(): void {
-    this.pokemonServices.getPokemons(this.offset(), this.limit()).subscribe({
-      next: (pokemons: Pokemon[]) => {
-        console.log('pokemons', pokemons);
-        this.pokemons.set(pokemons);
-      },
-      error: () => {},
-      complete: () => {},
-    });
+    this.getPokemons();
+  }
+
+  ngOnDestroy(): void {
+    this.servicesSubscription?.unsubscribe();
+  }
+
+  getPokemons() {
+    this.loading.set(true);
+    this.servicesSubscription = this.pokemonServices
+      .getPokemons(this.offset(), this.limit())
+      .subscribe({
+        next: (pokemons: Pokemon[]) => {
+          this.pokemons.update((old) => [...old, ...pokemons]);
+          this.offset.update((oldValue) => this.limit() + oldValue);
+        },
+        error: (error) => {
+          this.error.set(error);
+        },
+        complete: () => {
+          this.loading.set(false);
+        },
+      });
+  }
+
+  viewMore() {
+    this.getPokemons();
   }
 }
