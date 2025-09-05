96c48a7891b52a5e1b99abf8c91be4bbbcc776fc
Ricardo Villalobos Vargas <ricardo_villalobosv@hotmail.com>
2025-09-05 11:46:44 -0500

    Add skeleton stats component

---
+++ src/app/pages/pokemon-page/components/skeleton-stats/skeleton-stats.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-skeleton-stats',
+  imports: [],
+  templateUrl: './skeleton-stats.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class SkeletonStats {}
