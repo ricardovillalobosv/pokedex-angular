685cbc25ecdcdaea8c653f59f3e0d2385dc0b7d3
Ricardo Villalobos Vargas <ricardo_villalobosv@hotmail.com>
2025-09-05 11:47:50 -0500

    change skeleton

--- src/app/pages/pokemon-page/components/skeleton-card-pokemon-page/skeleton-card-pokemon-page.ts
+++ src/app/pages/pokemon-page/components/skeleton-card-pokemon-page/skeleton-card-pokemon-page.ts
@@ -4,9 +4,6 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   selector: 'app-skeleton-card-pokemon-page',
   imports: [],
   templateUrl: './skeleton-card-pokemon-page.html',
-  styleUrl: './skeleton-card-pokemon-page.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SkeletonCardPokemonPage {
-
-}
+export class SkeletonCardPokemonPage {}
