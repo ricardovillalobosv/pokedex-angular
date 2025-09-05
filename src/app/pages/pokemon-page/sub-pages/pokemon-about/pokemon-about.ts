1d61a627128b33f38a5d95d7acf2ddc89a2cf2c6
Ricardo Villalobos Vargas <ricardo_villalobosv@hotmail.com>
2025-09-05 11:47:07 -0500

    Add skeleton

--- src/app/pages/pokemon-page/sub-pages/pokemon-about/pokemon-about.ts
+++ src/app/pages/pokemon-page/sub-pages/pokemon-about/pokemon-about.ts
@@ -10,32 +10,47 @@ import {
 import { Pokemon } from '@core/models/pokemon.model';
 import { SharedPokemonDataService } from '@core/services/shared-pokemon-data';
 import { Subscription } from 'rxjs';
+import { SkeletonAbout } from '../../components/skeleton-about/skeleton-about';
 
 @Component({
   selector: 'app-pokemon-about',
-  imports: [UpperCasePipe],
+  imports: [UpperCasePipe, SkeletonAbout],
   templateUrl: './pokemon-about.html',
   styleUrl: './pokemon-about.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class PokemonAbout implements OnInit, OnDestroy {
   readonly pokemon = signal<Pokemon>({});
+  readonly loading = signal<boolean>(false);
 
   private sharedPokemonDataService = inject(SharedPokemonDataService);
 
   private serviceSubscription: Subscription | undefined;
+  private loadingSubscription: Subscription | undefined;
 
   ngOnInit(): void {
+    this.getLoading();
+    this.getData();
+  }
+
+  getLoading() {
+    this.loadingSubscription = this.sharedPokemonDataService.loading$.subscribe(
+      (loading) => {
+        this.loading.set(loading);
+      }
+    );
+  }
+
+  getData() {
     this.serviceSubscription = this.sharedPokemonDataService.pokemon$.subscribe(
-      {
-        next: (pokemon) => {
-          this.pokemon.set(pokemon);
-        },
+      (pokemon) => {
+        this.pokemon.set(pokemon);
       }
     );
   }
 
   ngOnDestroy(): void {
     this.serviceSubscription?.unsubscribe();
+    this.loadingSubscription?.unsubscribe();
   }
 }
