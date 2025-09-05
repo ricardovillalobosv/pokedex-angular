3c0c3e11e9cb6794ede161948abcc3bdb84d396d
Ricardo Villalobos Vargas <ricardo_villalobosv@hotmail.com>
2025-09-05 11:47:19 -0500

    Add skeleton

--- src/app/pages/pokemon-page/sub-pages/pokemon-stats/pokemon-stats.ts
+++ src/app/pages/pokemon-page/sub-pages/pokemon-stats/pokemon-stats.ts
@@ -8,36 +8,50 @@ import {
   signal,
 } from '@angular/core';
 import { Stat } from '@core/models/pokemon-result.model';
-import { Pokemon } from '@core/models/pokemon.model';
 import { SharedPokemonDataService } from '@core/services/shared-pokemon-data';
 import { Subscription } from 'rxjs';
+import { SkeletonStats } from '../../components/skeleton-stats/skeleton-stats';
 
 @Component({
   selector: 'app-pokemon-stats',
-  imports: [TitleCasePipe],
+  imports: [TitleCasePipe, SkeletonStats],
   templateUrl: './pokemon-stats.html',
   styleUrl: './pokemon-stats.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class PokemonStats implements OnInit, OnDestroy {
   readonly stats = signal<Stat[]>([]);
+  readonly loading = signal<boolean>(false);
 
   private sharedPokemonDataService = inject(SharedPokemonDataService);
 
   private serviceSubscription: Subscription | undefined;
+  private loadingSubscription: Subscription | undefined;
 
   ngOnInit(): void {
-    this.serviceSubscription = this.sharedPokemonDataService.pokemon$.subscribe(
-      {
-        next: (pokemon) => {
-          this.stats.set(pokemon.stats || []);
-        },
-      }
-    );
+    this.getLoading();
+    this.getData();
   }
 
   ngOnDestroy(): void {
     this.serviceSubscription?.unsubscribe();
+    this.loadingSubscription?.unsubscribe();
+  }
+
+  getData() {
+    this.serviceSubscription = this.sharedPokemonDataService.pokemon$.subscribe(
+      (pokemon) => {
+        this.stats.set(pokemon.stats || []);
+      }
+    );
+  }
+
+  getLoading() {
+    this.loadingSubscription = this.sharedPokemonDataService.loading$.subscribe(
+      (loading) => {
+        this.loading.set(loading);
+      }
+    );
   }
 
   buildWidth(value: number): string {
