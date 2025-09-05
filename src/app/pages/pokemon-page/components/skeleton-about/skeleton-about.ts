0667743db167b1470f2c6487e0f1c691c87e3ebf
Ricardo Villalobos Vargas <ricardo_villalobosv@hotmail.com>
2025-09-05 11:46:26 -0500

    Add skeleton about component

---
+++ src/app/pages/pokemon-page/components/skeleton-about/skeleton-about.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-skeleton-about',
+  imports: [],
+  templateUrl: './skeleton-about.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class SkeletonAbout {}
